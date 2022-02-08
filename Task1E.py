f43b79b831c7f43201b9fd698fc989751d76d2bc
Olgu Pilav <sop24@cam.ac.uk>
2022-02-08 22:38:04 +0000

    1e

--- Task1E.py
+++ Task1E.py
@@ -1,13 +1,14 @@
 from floodsystem.stationdata import build_station_list
 from floodsystem.geo import rivers_by_station_number
 
-def run():
-    stations = build_station_list()
-    print(rivers_by_station_number(stations, 9))
-
-
+def run ():
+    stations = build_station_list() 
+    n = 9
+    print(rivers_by_station_number(stations, n))
 if __name__ == "__main__":
+    print("")
     print("*** Task 1E: CUED Part IA Flood Warning System ***")
+    print("")
     run()
 
 
